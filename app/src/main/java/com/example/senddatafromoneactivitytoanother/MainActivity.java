a95024259ef14d628411f84ad7a0aea46121e4cf
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-08-05 15:43:40 +0500

    Send data from one activity to another in android first activity  is MainActivity and second activity is RecievingActivity

--- app/src/main/java/com/example/senddatafromoneactivitytoanother/MainActivity.java
+++ app/src/main/java/com/example/senddatafromoneactivitytoanother/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     // strings
     private String name;
-    private int age;
+    private String age;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
         etname = findViewById(R.id.etname);
         etage = findViewById(R.id.etage);
         btnsendData = findViewById(R.id.btnsend);
+
+        //getting data from edit text
+        name = etname.getText().toString().trim();
+        age = etage.getText().toString().trim();
 
         btnsendData.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,14 +49,19 @@ public class MainActivity extends AppCompatActivity {
     // function
     private void sendData() {
 
-        name = etname.getText().toString().trim();
-        age = Integer.parseInt(etage.getText().toString().trim());
+        if (etname.getText().toString().equals("")) {
+            etname.setError("Name is Required");
+        }else if (etage.getText().toString().equals("")){
+            etage.setError("Age is Required");
+        }else {
+
+            Intent i = new Intent(MainActivity.this, RecievingActivity.class);
+            i.putExtra(RecievingActivity.NAME,name);
+            i.putExtra(RecievingActivity.AGE,age);
+
+            startActivity(i);
+        }
 
 
-        Intent i = new Intent(MainActivity.this, RecievingActivity.class);
-        i.putExtra(RecievingActivity.NAME,name);
-        i.putExtra(RecievingActivity.AGE,age);
-
-        startActivity(i);
     }
 }
